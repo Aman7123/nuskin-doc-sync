351667750e112aaee7d505fc6e1c03178a2fb386
Aaron Renner <arennerwv@gmail.com>
2022-07-02 11:19:09 -0400

    New fetching feature

---
+++ fetch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+OUTPUT_LOCATION=/specs
+# List of URLs for specs
+RESOURCES=(
+    "https://raw.githubusercontent.com/Aman7123/image-sys-api-ac/main/src/generated/api/image-sys-api.yaml"
+    "https://raw.githubusercontent.com/Aman7123/rarity-sys-api-ac/main/src/generated/api/rarity-sys-api.yaml")
+
+# Loop through the array
+for i in "${RESOURCES[@]}"
+    do
+        echo "FETCHING $i"
+        # Download the file
+        curl -o $FQWN$OUTPUT_LOCATION/$(basename $i) $i
+    done
